6b6b656e3938cd7086230feb4ae553136c398671
albertusk95 <albertusk15@gmail.com>
2016-03-07 00:42:34 +0700

    board dari file

--- OOPworld/OOPworld/board.h
+++ OOPworld/OOPworld/board.h
@@ -1,27 +1,33 @@
-#pragma once
-
-#include "stdafx.h"
 #include "makhluk.h"
-#include <fstream>
+#include "Point.h"
 
-class board {
-public:
-	board();
-	void printboard();
+extern int sizex;
+extern int sizey;
+extern int sizepintu_p;
+extern Point min_h_vertical, min_k_vertical;
+extern Point max_h_vertical, max_k_vertical;
+extern Point min_h_horizontal, min_k_horizontal;
+extern Point max_h_horizontal, max_k_horizontal;
+extern char** isi;
+extern Point pintu_u;
+extern Point pintu_s;
+extern Point* pintu_p;
+		
+void board();
+	// make board dari file eksternal 
+void min_h_ver(char, int*, int*);
+void max_h_ver(char, int*, int*);
+void min_h_hor(char, int*, int*);
+void max_h_hor(char, int*, int*);
+void printboard();
 	//print kondisi dunia saat ini
-	void tambah(makhluk&);
+void tambah(makhluk&, char);
 	//tambahkan makhluk ke papan
-	void move(makhluk&);
+void move(makhluk&, char);
 	//gerakkan makhluk, parameter makhluk yang diminta parameter
 	//adalah sebelum digerakkan
 	//setelah move, makhluk akan berpindah
-	void hapus(makhluk&);
+void hapus(makhluk&);
 	//hapus makhluk dari papan
-	void tofile();
-	//cetak papan ke file csv
-
-protected:
-	const int sizex = 30;
-	const int sizey = 30;
-	char isi[30][30];
-};
+void deallocate_board();
+	//dealokasi array board
